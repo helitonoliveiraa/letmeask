b5cc17701cb698884da1b977b9cde0f729aeeb2d
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-23 15:30:04 -0300

    feat: create room on Realtime Database firebase

--- src/pages/NewRoom/index.tsx
+++ src/pages/NewRoom/index.tsx
@@ -1,14 +1,37 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+
+import { useAuth } from '../../hooks/useAuth';
 import { Input } from '../../components/Input';
+import { database } from '../../services/firebase';
 
 import illustrationImg from '../../assets/illustration.svg';
 import logoImg from '../../assets/logo.svg';
 
 import * as S from './styles';
-import { useAuth } from '../../contexts/AuthContext';
 
 export function NewRoom(): JSX.Element {
+  const [newRoom, setNewRoom] = useState('');
+
+  const history = useHistory();
   const { user } = useAuth();
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms');
+
+    const { key } = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    });
+
+    history.push(`/rooms/${key}`);
+  }
 
   return (
     <S.Container>
@@ -26,8 +49,13 @@ export function NewRoom(): JSX.Element {
 
           <strong>Crie uma nova sala</strong>
 
-          <form action="">
-            <Input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <Input
+              type="text"
+              placeholder="Nome da sala"
+              value={newRoom}
+              onChange={event => setNewRoom(event.target.value)}
+            />
 
             <S.CreateRoomButton>Entrar na sala</S.CreateRoomButton>
           </form>
