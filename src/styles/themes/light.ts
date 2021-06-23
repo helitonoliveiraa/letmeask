a4a75e2e9eed7fc61412c5340f6d918ed1133d4c
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-22 22:22:56 -0300

    chore: setup theme

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,21 @@
+export default {
+  colors: {
+    primary: '#007991',
+    secondary: '#222E50',
+    grandient: 'linear-gradient(#007991, #222e50)',
+
+    black: '#29292E',
+    shadow: '#050206',
+
+    grayDark: '#737380',
+    grayMedium: '#A8A8B3',
+    grayLight: '#DBDCDD',
+
+    danger: '#E73F5D',
+    red: '#EA4335',
+
+    background: '#F8F8F8',
+    details: '#FEFEFE',
+    white: '#FFFFFF',
+  },
+};
