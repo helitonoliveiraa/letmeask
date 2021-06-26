fdbfcde447a0563f5e07f6f69f35fd87f39c47e8
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-06-26 14:34:12 -0300

    feat: create theme context

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, ReactNode, useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import { light, dark } from '../styles/themes/index';
+
+type Theme = 'light' | 'dark';
+
+export type ThemeContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+
+type ThemeProviderProps = {
+  children: ReactNode;
+};
+
+export const ThemeContext = createContext<ThemeContextType>(
+  {} as ThemeContextType,
+);
+
+export function ThemeContextProvider({
+  children,
+}: ThemeProviderProps): JSX.Element {
+  const [theme, setTheme] = useState<Theme>('light');
+
+  function toggleTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  }
+
+  const currentTheme = theme === 'light' ? light : dark;
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        {children}
+      </ThemeContext.Provider>
+    </ThemeProvider>
+  );
+}
